669a4143a3dac05d11abf5cf035b450892d68d5f
nicatkazimli <kazimlin08@gmail.com>
2025-11-30 20:46:43 +0400

    first changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CategoryPage from "./pages/CategoryPage";
 
+
 function App() {
   return (
     <Router>
